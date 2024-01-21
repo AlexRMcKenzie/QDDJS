c1f00f527f3b4d31c198c41f9338f2fe8b5d1f4f
Alex McKenzie <alexmckenzie1990@gmail.com>
2024-01-21 18:25:08 +0000

    Altered dateOfBirth variable

--- age-in-days.js
+++ age-in-days.js
@@ -1,5 +1,5 @@
-var dateOfBirth = new Date();
-console.log("Your age in years is " + dateOfBirth)
+var dateOfBirth =
+  console.log("Your age in years is " + dateOfBirth)
 
 var ageInDays = dateOfBirth * 365;
 console.log("Your age in days is " + ageInDays)
